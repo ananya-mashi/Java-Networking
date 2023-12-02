8f3982fb233444a4a1dc406b46198a378f4c9d15
Ananya <60173002+ananya-mashi@users.noreply.github.com>
2023-12-02 07:37:43 +0530

    Update DBConnection.java

--- techciti/src/com/ananya/db/DBConnection.java
+++ techciti/src/com/ananya/db/DBConnection.java
@@ -10,7 +10,7 @@ public static Connection createConnection()
     Connection con = null;
     String url = "jdbc:mysql://localhost:3306/techciti"; 
     String username = "root";
-    String password = "gunarama";
+    String password = "";
     
     try 
     {
